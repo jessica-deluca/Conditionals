3be55d1f1693c80c04c00871d404194f110caff6
Your Name <you@example.com>
2017-06-28 13:46:31 -0400

    adding questions from class

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -9,7 +9,31 @@ namespace Conditionals
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //ask number of people in party
+            Console.WriteLine("How many people are in your party?");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            //ask total bill of party
+            Console.WriteLine("What is your total bill?");
+            double totalBill = double.Parse(Console.ReadLine());
+
+            //state variables
+            double billWithDiscount;
+            double costPerPerson;
+
+            //give the discounts based on total bill of party
+            if (totalBill >= 50)
+            {
+                billWithDiscount = totalBill * .9;
+            }
+            else
+            {
+                billWithDiscount = totalBill * .95;
+            }
+
+            //find cost per person and print receipt
+            costPerPerson = billWithDiscount / numberOfPeople;
+            Console.WriteLine("Cost Per Person: " + costPerPerson);
         }
     }
 }
